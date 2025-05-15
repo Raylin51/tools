26bfd8e40bff9993811f2cdf7fa65966b5e355ca
Decode <decode@Decode-Pro.local>
2025-05-15 19:21:58 +0800

    update

--- src/scan.ts
+++ src/scan.ts
@@ -105,13 +105,14 @@ const handleTokenCreation = async (
 
   // 检查是不是大 V
   const cid = uri.split('/').pop()?.trim()
-  // const metadataRes = await axios.get(`https://ipfs.io/ipfs/${cid}`, {
+  // 开始获取 metadata 内容
+  const fetchMetadataStart = Date.now()
+    // const metadataRes = await axios.get(`https://ipfs.io/ipfs/${cid}`, {
   //   headers: {
   //     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
   //   "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
   //   }
   // })
-  const fetchMetadataStart = Date.now()
   const { verifiedFetch } = await import('@helia/verified-fetch')
   const res = await verifiedFetch(`ipfs://${cid}`).catch(error => console.log(`Can't fetch ${uri}`)) 
   console.log(`Fetch metadata: ${Date.now() - fetchMetadataStart}ms`)
